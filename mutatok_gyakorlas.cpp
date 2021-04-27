16a2a54476ee86a6938ca96d6c928af24041af12
tbalazs1213 <83310872+tbalazs1213@users.noreply.github.com>
2021-04-27 22:10:25 +0200

    Create mutatok_gyakorlas.cpp
    Készítsen egy struktúrát, amely koordinátákat tárol (x, y, z)!
    Hozzon létre egy tízelemű tömböt, amely koordinátákat tárol!
    Egy mutató segítségével töltse fel a tömböt értékekkel!
    A mutatón keresztül jelenítse meg a tömb elemeit!

---
+++ mutatok_gyakorlas.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+#define MIN 0
+#define MAX 10
+
+using namespace std;
+
+struct koordinata 
+{
+  int x,y,z;
+};
+
+int main() 
+{
+	srand(time(NULL));
+  koordinata tomb[10];
+  for(int i=0; i<10; i++) 
+  {
+		(tomb + i)->x = MIN + rand()%(MAX-MIN+1);
+		(tomb + i)->y = MIN + rand()%(MAX-MIN+1);
+		(tomb + i)->z = MIN + rand()%(MAX-MIN+1);
+		cout << (tomb + i)->x << '\t' << (tomb + i)->y << '\t' << (tomb + i)->z << '\n';
+	}
+} 
