98f18ebc682370a8b8d97d8d1bee6761dc518281
tbalazs1213 <83310872+tbalazs1213@users.noreply.github.com>
2021-06-11 22:08:14 +0200

    Update mutatok_gyakorlas.cpp

--- mutatok_gyakorlas.cpp
+++ mutatok_gyakorlas.cpp
@@ -12,13 +12,13 @@ struct koordinata
 
 int main() 
 {
-	srand(time(NULL));
+  srand(time(NULL));
   koordinata tomb[10];
   for(int i=0; i<10; i++) 
   {
-		(tomb + i)->x = MIN + rand()%(MAX-MIN+1);
-		(tomb + i)->y = MIN + rand()%(MAX-MIN+1);
-		(tomb + i)->z = MIN + rand()%(MAX-MIN+1);
-		cout << (tomb + i)->x << '\t' << (tomb + i)->y << '\t' << (tomb + i)->z << '\n';
-	}
+	(tomb + i)->x = MIN + rand()%(MAX-MIN+1);
+	(tomb + i)->y = MIN + rand()%(MAX-MIN+1);
+	(tomb + i)->z = MIN + rand()%(MAX-MIN+1);
+	cout << (tomb + i)->x << '\t' << (tomb + i)->y << '\t' << (tomb + i)->z << '\n';
+  }
 } 
